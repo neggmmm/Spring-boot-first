c7704609ff09c364c7c04ef4b1d402ae52d759f0
neggmmm <kekkdead@gmail.com>
2026-05-30 10:39:46 +0300

    CRUD operations (controller, service, repository)

---
+++ src/main/java/com/example/PriceAlerter/modules/users/UsersController.java
@@ -0,0 +1,52 @@
+package com.example.PriceAlerter.modules.users;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    private final UsersServices usersServices;
+
+    @Autowired
+    public UsersController(UsersServices usersServices) {
+        this.usersServices = usersServices;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return usersServices.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return usersServices.getUserById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createUser(@RequestBody User user) {
+        return usersServices.createUser(user);
+    }
+
+    @PatchMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return usersServices.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable Long id) {
+        usersServices.deleteUser(id);
+    }
+}
