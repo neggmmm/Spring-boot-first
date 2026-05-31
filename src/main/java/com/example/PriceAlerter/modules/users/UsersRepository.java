444dac77f902ebc8a48e321373dc35db29684336
neggmmm <kekkdead@gmail.com>
2026-05-31 19:32:26 +0300

    add exists email to usersRepo

--- src/main/java/com/example/PriceAlerter/modules/users/UsersRepository.java
+++ src/main/java/com/example/PriceAlerter/modules/users/UsersRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsersRepository extends JpaRepository<User, UUID> {
+    boolean existsByEmailIgnoreCase(String email);
 }
