809b21fd595a8cabbabb6e64c0e44c22df396f19
neggmmm <kekkdead@gmail.com>
2026-06-01 06:59:51 +0300

    add login Response and login Request dtos

---
+++ src/main/java/com/example/PriceAlerter/modules/authentication/dto/LoginRequest.java
@@ -0,0 +1,26 @@
+package com.example.PriceAlerter.modules.authentication.dto;
+
+public class LoginRequest {
+    private String email;
+    private String password;
+
+    public LoginRequest(String email, String password){
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email; 
+    }
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPasword(String password){
+        this.password = password;
+    }
+}
